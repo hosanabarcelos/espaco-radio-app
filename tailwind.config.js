400257b163f52412f1a02b9f30e88d1a2958d46d
Hosana <hosanabarceloscandido@gmail.com>
2025-02-13 15:20:55 -0300

    feat: add configuração de tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: "class",
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "var(--color-primary)",
+        secondary: "var(--color-secondary)",
+        background: "var(--color-background)",
+        text: "var(--color-text)"
+      },
+    },
   },
   plugins: [],
-}
-
-
+};

ad703ecb604864869a349605d77d450c7bc6f042
Hosana <hosanabarceloscandido@gmail.com>
2025-02-15 20:20:55 -0300

    feat: importa e configura phosphor e tailwind

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,17 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import PhosphorIcons from "@phosphor-icons/vue"
 
 import App from './App.vue'
 import router from './router'
 
+import "tailwindcss/tailwind.css";
+import './assets/fonts.css';
+
 const app = createApp(App)
 
+app.use(PhosphorIcons)
 app.use(createPinia())
 app.use(router)
 

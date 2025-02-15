de790e450720f610934bb4aa587dbd73aecf666e
Hosana <hosanabarceloscandido@gmail.com>
2025-02-15 20:18:25 -0300

    feat; ajusta config de tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: "class",
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}"
-  ],
-  theme: {
-    extend: {
-      colors: {
-        primary: "var(--color-primary)",
-        secondary: "var(--color-secondary)",
-        background: "var(--color-background)",
-        text: "var(--color-text)"
-      },
-    },
-  },
-  plugins: [],
+	darkMode: "class",
+	content: [
+		"./index.html",
+		"./src/**/*.{vue,js,ts,jsx,tsx}"
+	],
+	theme: {
+		extend: {
+			colors: {
+				primary: "var(--color-primary)",
+				secondary: "var(--color-secondary)",
+				background: "var(--color-background)",
+				text: "var(--color-text)"
+			},
+			fontFamily: {
+				trench: ['Trench Thin', 'sans-serif'],
+			},
+		},
+	},
+	plugins: [],
 };

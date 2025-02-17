f4d4d640ecd22883aaecb812c2cfa77627b76e1f
Hosana <hosanabarceloscandido@gmail.com>
2025-02-16 21:51:58 -0300

    fix: corrige fonte trench

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: "class",
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	darkMode: "class",
+	content: [
+		"./index.html",
+		"./src/**/*.{vue,js,ts,jsx,tsx}"
+	],
+	theme: {
+			fontFamily: {
+				trench: ['Trench Thin', 'sans-serif'],
+			},
+		},
+	plugins: [],
 };

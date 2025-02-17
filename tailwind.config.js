d6a784e829952e404522ffc2b7e63990ca8395eb
Hosana <hosanabarceloscandido@gmail.com>
2025-02-16 21:37:32 -0300

    fix: corrige erros de build

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	darkMode: "class",
-	content: [
-		"./index.html",
-		"./src/**/*.{vue,js,ts,jsx,tsx}"
-	],
-	theme: {
-		extend: {
-			colors: {
-				primary: "var(--color-primary)",
-				secondary: "var(--color-secondary)",
-				background: "var(--color-background)",
-				text: "var(--color-text)"
-			},
-			fontFamily: {
-				trench: ['Trench Thin', 'sans-serif'],
-			},
-		},
-	},
-	plugins: [],
+  darkMode: "class",
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
@@ -15,4 +13,16 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'; // Separa dependências externas
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000, // Opcional: Reduz os avisos para 1MB
+  },
+});

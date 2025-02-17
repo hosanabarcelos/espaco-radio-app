b15d0cf4b3bdd13266abc0c73d118f3c98929442
Hosana <hosanabarceloscandido@gmail.com>
2025-02-17 10:00:11 -0300

    fix: corrige erro de build

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	darkMode: "class",
-	content: [
-		"./index.html",
-		"./src/**/*.{vue,js,ts,jsx,tsx}",
-		"./src/views/**/*.{vue,js,ts,jsx,tsx}",
-		"./src/components/**/*.{vue,js,ts,jsx,tsx}",
-	],
-	theme: {
-		fontFamily: {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+    fontFamily: {
 			trench: ['Trench Thin', 'sans-serif'],
 		},
-	},
-	plugins: [],
-};
+  },
+  plugins: [],
+}
+
